f74e0ca7d38483f7414cf5bdd11f772c8026400c
Logan <LSArmstrong01@indianatech.net>
2026-03-13 20:26:59 -0400

    yfinace Changes

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,22 +1,21 @@
-import requests
-import json
+import yfinance as yf
 import os
 
-API_KEY = "OCXQ78XIZ2FCGO3J"
 SYMBOL = "IBM"
 
 def fetch_stock_data():
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=OCXQ78XIZ2FCGO3J'
-    response = requests.get(url)
-    data = response.json()
     
+    df = yf.download(
+        SYMBOL,
+        start="2015-01-01",
+        end="2024-01-01",
+        interval="1d"
+    )
+
     os.makedirs("../data", exist_ok=True)
-    with open("../data/raw_data.json", "w") as f:
-        json.dump(data, f, indent=2)
+
+    df.to_csv("../data/raw_stock_data.csv")
+
+    print("Stock data downloaded and saved.")
     
-    print("Stock data fetched and saved.")
-    return data
-
-
-
-
+    return df
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,22 +1,16 @@
 import pandas as pd
-import json
 
 def load_and_clean_data():
-    with open("../data/raw_data.json") as f:
-        data = json.load(f)
-    
-    ts = data["Time Series (Daily)"]
-    
-    df = pd.DataFrame.from_dict(ts, orient='index')
-    df = df[["1. open", "2. high", "3. low", "4. close", "5. volume"]]
-    df.columns = ["open", "high", "low", "close", "volume"]
-    
-    # Convert to numeric safely
-    for col in df.columns:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
-    
-    df.index = pd.to_datetime(df.index)
-    df.sort_index(inplace=True)
+
+    df = pd.read_csv("../data/raw_stock_data.csv")
+
+    df["Date"] = pd.to_datetime(df["Date"])
+    df.set_index("Date", inplace=True)
+
+    df = df[["Open","High","Low","Close","Volume"]]
+
+    df.columns = ["open","high","low","close","volume"]
+
     df.dropna(inplace=True)
-    
+
     return df
